72d4cee8e5e8dbac70df59f95402a62d28aab660
ae <ae@corp2.net>
2019-04-22 00:00:12 +0300

    'seeds'

--- database/seeds/BlogCategoriesTableSeeder.php
+++ database/seeds/BlogCategoriesTableSeeder.php
@@ -20,7 +20,7 @@ class BlogCategoriesTableSeeder extends Seeder
             'parent_id' => 0,
         ];
 
-        for ($i=1; $i <= 10 ; $i+1) { 
+        for ($i = 1; $i <= 10; $i++) { 
             $cName = 'Категория #'.$i;
             $parentid = ($i > 4) ? rand(1, 4) : 1;
 
@@ -29,8 +29,8 @@ class BlogCategoriesTableSeeder extends Seeder
                 'slug'      => str_slug($cName),
                 'parent_id' => $parentid,
             ];
-
-            \DB::table('blog_categories')->insert($categories);
         }
+        
+        \DB::table('blog_categories')->insert($categories);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name'      => 'Автор не известен',
+                'email'     => 'author_unknown@g.g',
+                'password'  => bcrypt(str_random(16)),
+            ],
+            [
+                'name'      => 'Автор',
+                'email'     => 'author@g.g',
+                'password'  => bcrypt(123123),
+            ],
+        ];
+
+        DB::table('users')->insert($data);
     }
 }
