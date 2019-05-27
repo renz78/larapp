9bd8f626e17240faf1510af19d5950011b0cbf27
ae <ae@corp2.net>
2019-05-27 22:46:13 +0300

    'test11'

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use Illuminate\Http\Request;
 use App\Models\BlogCategory;
+use App\Http\Requests\BlogCategoryUpdateRequest;
 
 class CategoryController extends BaseController
 {
@@ -74,9 +75,14 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {   
-        //$id = 234234234;
+        // $rules = [
+        //     'title'         => 'required|min:5|max:200',
+        //     'slug'          => 'max:200',
+        //     'description'   => 'string|max:500|min:3',
+        //     'parent_id'     => 'required|integer|exists:blog_categoryes,id',
+        // ];
         $item = BlogCategory::find($id);
         //dd($item);
         
